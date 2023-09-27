c4f4cc0b06340551c50197e338253f581fdd71fc
tobionesies <d2ba5@pm.me>
2023-09-27 10:38:24 +0200

    changed routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,19 +2,19 @@ const router = require('express').Router();
 const userController = require('../controllers/user_controller.js')
 
 //Create from posted json
-router.post("/", userController.create_user);
+router.post("/user", userController.create_user);
 
 //Get all
-router.get("/", userController.get_all_users);
+router.get("/users", userController.get_all_users);
 
 
 //Get by ID
-router.get("/:id", userController.get_user);
+router.get("/user/:id", userController.get_user);
 
 //Update by ID
-router.put("/:id", userController.put_user);
+router.put("/user/:id", userController.put_user);
 
 //Delete by ID
-router.delete("/:id", userController.delete_user);
+router.delete("/user/:id", userController.delete_user);
 
 module.exports = router;
