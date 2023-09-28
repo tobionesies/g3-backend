62103a4d13b5beb35430d8e56b581e2bdf2f0d0b
tobionesies <d2ba5@pm.me>
2023-09-28 09:44:29 +0200

    add firebase-functions

--- functions/server.js
+++ functions/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.json({status: "ok"});
-});
+// app.get('/', (req, res) => {
+//    res.json({status: "ok"});
+// });
 
 app.use('/api/user', require('./routes/user_routes.js'));
 
